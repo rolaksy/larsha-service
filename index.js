ed0df1afd48c1d96b12123bd1d4bc772470e43a3
laks-yalamati <9laksy@gmail.com>
2024-07-27 01:49:58 +1200

    Implement middleware api

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js
+const express = require('express');
+const app = express();
+const userApi = require('./api/user');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+// Middleware to check the Origin or Referer header
+const checkOrigin = (req, res, next) => {
+  const allowedOrigin = 'http://localhost:3001';
+  const origin = req.headers.origin || req.headers.referer;
+  if (origin && (origin === allowedOrigin || origin.startsWith(allowedOrigin))) {
+    next();
+  } else {
+    res.status(403).json({ message: 'Access denied' });
+  }
+};
+
+// Apply the middleware to your API routes
+app.use('/api', checkOrigin);
+
+//different apis
+app.use('/api/users', userApi);
+
+//normal index route
+app.get('/', async (req, res) => {
+  res.status(200).send("<h1>Server is UP</h1>");
+});
+
+//server start
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ test.js
@@ -0,0 +1,96 @@
+const dotenv = require('dotenv');
+const Gitrows = require("./lib/gitrows");
+
+let gitrowsOptions = {
+    author: {
+        name: 'rolaksy',
+        email: '9laksy@gmail.com'
+    },
+    owner: 'rolaksy',
+    user: 'rolaksy',
+    token: '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',
+    branch: 'master',
+    csv: { delimiter: ',' },
+    cacheTTL: 1000, // You can tweak this value to have a custom cache validity time (3s here)
+    type: 'json' //yaml, csv, json
+};
+
+const gitrows = new Gitrows(gitrowsOptions);
+
+//working tested
+async function select(table, filters) {
+    console.log('select started.');
+    await gitrows.get(
+        '@github/rolaksy/larsha-store/'+table+'/o.json',
+        filters
+    ).then(res => {console.log(res)}).catch(e => {console.log(e)});
+}
+
+//working tested
+async function insert(table, data) {
+    console.log('insert started.');
+    await gitrows.put(
+        '@github/rolaksy/larsha-store/'+table+'/o.json',
+        data
+    ).then(res => {console.log(res)}).catch(e => {console.log(e)});
+}
+
+//working tested
+async function create(table) {
+    console.log('create started.');
+    await gitrows.create(
+        '@github/rolaksy/larsha-store/'+table+'/o.json',
+        []
+    ).then(res => {console.log(res)}).catch(e => {console.log(e)});
+}
+
+
+async function update(table, data, filter) {
+    console.log('update started.');
+    await gitrows.update(
+        '@github/rolaksy/larsha-store/'+table+'/o.json',
+        data,
+        filter
+    ).then(res => {console.log(res)}).catch(e => {console.log(e)});
+}
+
+async function remove(table, filter) {
+    console.log('update started.');
+    await gitrows.delete(
+        '@github/rolaksy/larsha-store/'+table+'/o.json',
+        filter
+    ).then(res => {console.log(res)}).catch(e => {console.log(e)});
+}
+
+async function drop(table) {
+    console.log('update started.');
+    await gitrows.drop(
+        '@github/rolaksy/larsha-store/'+table+'/o.json'
+    ).then(res => {console.log(res)}).catch(e => {console.log(e)});
+}
+
+
+async function handleRequest()
+{
+    console.log('handleRequest started.');
+    const dataToInsert = {
+        "name": "Laks Yalamati",
+        "email": "xxxx@gmail.com",
+        "__tt__": 0
+    };
+
+    create('ks-users');
+    
+    //drop('ks-users');
+    //insert('ks-users', dataToInsert);
+
+    //remove('ks-users', {"id": 101});
+    
+    //update('users', dataToInsert, {"id": "100"});
+
+    //select('users', {"email": "9laksy@gmail.com"});
+}
+
+
+
+handleRequest();
